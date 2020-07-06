f12706ffcc32cdd1f7d645f240878704e49e04be
Natalie Pina <natalie.a.pina@gmail.com>
2020-07-06 17:06:22 -0500

    add click event for handling selected tiles

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import OptionsPanel from '../OptionsPanel'
 import Board from '../Board'
-import { createTiles } from '../../misc/utils';
+import { createTiles, indexOfSelected } from '../../misc/utils';
 
 import './App.css';
 
@@ -13,7 +13,7 @@ class App extends Component{
     this.state = {
       numTiles: 36,
       playing: false,
-      previousTileIndex: null,
+      previousTile: null,
       tiles: [],
       toBeCleared: null
     }
@@ -22,9 +22,43 @@ class App extends Component{
   startGame = (numTiles) => {
     this.setState({
       playing: true,
-      previousTileIndex: null,
       toBeCleared: null,
-      tiles: createTiles(this.state.numTiles)
+      tiles: createTiles(this.state.numTiles, this.handleTileClicked)
+    })
+  }
+
+  handleTileClicked = (id, color) => {
+    const tiles = Array.from(this.state.tiles)
+    const selectedTileIndex = indexOfSelected(this.state.tiles, id, color)
+    const previousTile = this.state.previousTile
+    const toBeCleared = this.state.toBeCleared
+
+    if (toBeCleared) {
+      tiles[toBeCleared[0]].selected = false
+      tiles[toBeCleared[1]].selected = false
+    }
+
+    tiles[selectedTile].selected = true
+
+    if(previousTileIndex !== null) {
+
+      if(tiles[previousTile].color === color && tiles[previousTile].id !== id) {
+        tiles[previousTile].matched = true 
+        tiles[selectedTile].matched = true 
+        previousTile= null
+      } else {
+        toBeCleared = [previousTile, selectedTileIndex]
+        previousTile = null
+      }
+
+    } else {
+      previousTile = selectedTileIndex
+    }
+
+    this.setState({
+      previousTile,
+      tiles,
+      toBeCleared
     })
   }
  
--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -1,6 +1,6 @@
 import React from 'react'
-
 import './Tile.css'
+
 
 const Tile = (props) => {
   const { selected, matched, color } = props;
@@ -9,7 +9,7 @@ const Tile = (props) => {
   const svg = <props.svg />
 
   return (
-    <div className='Tile' style={dynamicColor}>
+    <div className='Tile' style={dynamicColor} onClick={() => handleTileClicked(id, color)}>
       {selected || matched ? svg : null}
     </div>
   )
