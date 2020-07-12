7411fd9fd68b74ee1af19f0ece4bfe075173afd7
Natalie Pina <natalie.a.pina@gmail.com>
2020-07-12 18:52:37 -0500

    add refactor

---
+++ refactor/components/Tile/Tile.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Tile.css'
+
+
+const Tile = (props) => {
+  const { selected, matched, color, handleTileClicked, id } = props;
+
+  const dynamicColor = (selected || matched) ? {backgroundColor: color} : null;
+  const svg = <props.svg />
+
+  return (
+    <div className='Tile' style={dynamicColor} onClick={() => handleTileClicked(id, color)}>
+      {selected || matched ? svg : null}
+    </div>
+  )
+}
+
+export default Tile
